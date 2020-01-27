e2d1e91e2f724c43a8350c21abb5b16767feb0c4
Daniel Vasilev <DVasilev@walmart.com>
2020-01-27 13:09:30 -0500

    search term clean up

--- app/model/Utils.scala
+++ app/model/Utils.scala
@@ -3,7 +3,9 @@ package model
 object Utils {
   implicit class StringUtils(val string: String) extends AnyVal {
     def cleanseSearchTerms: String = {
-      string.toLowerCase.filter(char => char.isLetterOrDigit || char.isSpaceChar).stripSuffix(" ")
+      string.toLowerCase.toArray.map(char =>
+        if (!char.isLetterOrDigit || !char.isSpaceChar == ' ') ' ' else char).mkString.replaceAll("[ ]+", " ")
+        .stripSuffix(" ")
     }
 
     def containsSpecialCharacters: Boolean = string.toLowerCase.exists(char => !char.isLetterOrDigit && !char.isSpaceChar)
--- app/model/services/ServiceRequestExecutor.scala
+++ app/model/services/ServiceRequestExecutor.scala
@@ -7,7 +7,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 case class ServiceRequestExecutor[T](request: WSRequest)(implicit ec: ExecutionContext) {
   def execute(f: JsValue => T)(implicit reads: Reads[T]): Future[T] = {
-    println(request.uri.toString)
     request.get().map(_.json).map(json => f(json))
   }
 }
