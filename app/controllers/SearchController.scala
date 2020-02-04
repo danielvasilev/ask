6dcab229d00cf9b2a8e25ae6377dc376ec606248
Daniel Vasilev <DVasilev@walmart.com>
2020-02-04 10:58:21 -0500

    refactoring code to the service class from the controller

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -5,7 +5,6 @@ import javax.inject._
 import play.api.libs.ws._
 import play.api.mvc._
 
-import model.Utils.StringUtils
 import model.search.SearchTerms
 import model.services.data.Movie
 import model.services.data.Movie.EmptyMovie
@@ -13,7 +12,6 @@ import model.services.{MovieDataService, ServiceRequestExecutor}
 
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Random
 
 
 @Singleton
@@ -26,33 +24,9 @@ class SearchController @Inject()(val controllerComponents: ControllerComponents,
     val serviceResponse = ServiceRequestExecutor[Movie](searchRequest).execute(Movie.fromJson)
     serviceResponse.flatMap {
       case EmptyMovie => Future.successful(Ok(views.html.index(s"[${searchTerms.toString}] is a dead end, try other search terms.")))
-      case response: Movie => movieResult(response, movieDataService)
-    }
-  }
-
-  private def movieResult(responseMovie: Movie, movieDataService: MovieDataService)(implicit ec: ExecutionContext): Future[Result] = {
-    val plotWords = responseMovie.plot.split(" ").filterNot(_.containsSpecialCharacters) :+ responseMovie.country
-    val relatedSearchTerms = getRelatedSearchTerms(plotWords)
-    val relatedMoviesFutures = relatedSearchTerms.map { relatedTerms =>
-      val relatedSearchRequest = movieDataService.search(relatedTerms)
-      ServiceRequestExecutor[Movie](relatedSearchRequest).execute(Movie.fromJson)
-    }
-    val moviesResult = Future.sequence(relatedMoviesFutures).map(_ :+ responseMovie)
-    moviesResult.map { response =>
-      val moviesToRender = response.filterNot(_ == EmptyMovie).distinctBy(_.title).sortBy(_.defaultRating)(Ordering.Int.reverse)
-      Ok(views.html.searchPage(moviesToRender))
-    }
-  }
-
-  private def getRelatedSearchTerms(plotWords: Array[String], acc: List[SearchTerms] = Nil, length: Int = 9, cycles: Int = 99): Seq[SearchTerms] = {
-    if (acc.length == length || plotWords.isEmpty || cycles == 0)
-      acc
-    else {
-      def randomIndex = Random.between(0, plotWords.length)
-
-      val randomNumberTerms = Random.between(1, 3)
-      val searchTerms = Seq(plotWords(randomIndex), plotWords(randomIndex)).take(randomNumberTerms).sorted
-      getRelatedSearchTerms(plotWords, (acc :+ SearchTerms(searchTerms)).distinct, cycles = cycles - 1)
+      case response: Movie => movieDataService.movieResult(response, movieDataService).map { movies =>
+        Ok(views.html.searchPage(movies))
+      }
     }
   }
 }
--- app/model/services/MovieDataService.scala
+++ app/model/services/MovieDataService.scala
@@ -5,9 +5,13 @@ import javax.inject.Inject
 import akka.http.scaladsl.model.Uri
 import play.api.libs.ws._
 
+import model.Utils.StringUtils
 import model.search.SearchTerms
+import model.services.data.Movie
+import model.services.data.Movie.EmptyMovie
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Random
 
 case class MovieDataService @Inject()(ws: WSClient) {
   private val link = Uri("http://www.omdbapi.com")
@@ -19,5 +23,30 @@ case class MovieDataService @Inject()(ws: WSClient) {
     ))
     val url = link.withQuery(query)
     ws.url(url.toString)
+  }
+
+  def movieResult(responseMovie: Movie, movieDataService: MovieDataService)(implicit ec: ExecutionContext): Future[Seq[Movie]] = {
+    val plotWords = responseMovie.plot.split(" ").filterNot(_.containsSpecialCharacters) :+ responseMovie.country
+    val relatedSearchTerms = getRelatedSearchTerms(plotWords)
+    val relatedMoviesFutures = relatedSearchTerms.map { relatedTerms =>
+      val relatedSearchRequest = movieDataService.search(relatedTerms)
+      ServiceRequestExecutor[Movie](relatedSearchRequest).execute(Movie.fromJson)
+    }
+    val moviesResult = Future.sequence(relatedMoviesFutures).map(_ :+ responseMovie)
+    moviesResult.map { response =>
+      response.filterNot(_ == EmptyMovie).distinctBy(_.title).sortBy(_.defaultRating)(Ordering.Int.reverse)
+    }
+  }
+
+  private def getRelatedSearchTerms(plotWords: Array[String], acc: List[SearchTerms] = Nil, length: Int = 9, cycles: Int = 99): Seq[SearchTerms] = {
+    if (acc.length == length || plotWords.isEmpty || cycles == 0)
+      acc
+    else {
+      def randomIndex = Random.between(0, plotWords.length)
+
+      val randomNumberTerms = Random.between(1, 3)
+      val searchTerms = Seq(plotWords(randomIndex), plotWords(randomIndex)).take(randomNumberTerms).sorted
+      getRelatedSearchTerms(plotWords, (acc :+ SearchTerms(searchTerms)).distinct, cycles = cycles - 1)
+    }
   }
 }
